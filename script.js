b773c6d98be081b430647e85e07ab62330ad4754
yugmani <yugmani@yahoo.com>
2021-08-15 23:32:40 -0700

    scroll behavior in js file added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hello!')
+document.getElementById('nav-bar').addEventListener('click', function(e) {
+  e.preventDefault();
+  const target = e.target;
+
+  if (target.classList.contains('link-a')) {
+    const id = target.getAttribute('href').slice(1);
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
